339047992bf599338369a83bd78f8b82e29b1a95
Srijan Ayer <ayersrijan10@gmail.com>
2025-07-07 10:59:26 +0545

    Updated BaseTest

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -3,6 +3,7 @@ package tests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.*;
 import pages.LoginPage;
 import utils.ConfigReader;
@@ -13,18 +14,25 @@ public class BaseTest {
     @BeforeClass
     public void setUp() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless"); // Optional for CI
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--remote-allow-origins=*");
+
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.get(ConfigReader.get("URL"));
     }
 
     @Test
     public void loginTest() {
-        driver.get(ConfigReader.get("URL"));
         LoginPage loginPage = new LoginPage(driver);
         loginPage.enterUsername(ConfigReader.get("USERNAME"));
         loginPage.enterPassword(ConfigReader.get("PASSWORD"));
         loginPage.clickLogin();
-
-        // Optional: Add assertions here
     }
 
     @AfterClass
